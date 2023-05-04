3801605ac6e18546dedeb801c07a30a98b5ca7db
quangchien2203 <chien.dang@digitalfortress.dev>
2023-05-04 14:12:58 +0700

    update deploy github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,8 +4,8 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const organizationName = "docsrubix";
-const projectName = "docsrubix";
+const organizationName = "NubeIO";
+const projectName = "rubix-ce-docs";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
