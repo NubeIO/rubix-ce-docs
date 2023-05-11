87cb6974692c8e9d11bb6f977a3b3d0e375de61d
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2023-05-11 09:35:30 +0700

    update review image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'https://nube-io.com/wp-content/uploads/2022/04/Nube-logo.png',
       navbar: {
         title: 'Home Page',
         logo: {
